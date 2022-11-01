450fd1fcbcc39ad8b8465f8f71a2c839fe67a0c4
zhangzhengjie <z17681406163@163.com>
2022-11-01 19:57:46 +0800

    demo

---
+++ model_re/demo.py
@@ -0,0 +1,7 @@
+import json
+import medical_re
+model4s, model4po = medical_re.load_model()
+
+text = '据报道称，新冠肺炎患者经常会发热、咳嗽，少部分患者会胸闷、乏力，其病因包括: 1.自身免疫系统缺陷\n2.人传人。'  # content是输入的一段文字
+res = medical_re.get_triples(text, model4s, model4po)
+print(json.dumps(res, ensure_ascii=False, indent=True))
